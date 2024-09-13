8a5baf6a7326c81f534ba7f4bce339e6071da456
Matus Pavlasek <matus.pavlasek@luigisbox.com>
2024-09-13 15:17:03 +0200

    Make integration compatible with older Magento versions

--- Model/Resolver/Product/ProductCustomAttributesLuigi.php
+++ Model/Resolver/Product/ProductCustomAttributesLuigi.php
@@ -7,28 +7,36 @@ declare(strict_types=1);
 
 namespace Luigisbox\Integration\Model\Resolver\Product;
 
-use Magento\Catalog\Api\Data\ProductAttributeInterface;
-use Magento\Catalog\Model\FilterProductCustomAttribute;
-use Magento\Catalog\Model\Product;
-use Magento\CatalogGraphQl\Model\ProductDataProvider;
-use Magento\Eav\Api\Data\AttributeInterface;
-use Magento\EavGraphQl\Model\Output\Value\GetAttributeValueInterface;
-use Magento\EavGraphQl\Model\Resolver\GetFilteredAttributes;
+/**
+ * Requirements for GraphQL
+ */
 use Magento\GraphQl\Model\Query\ContextInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 
 /**
+ * Requirements to handle custom attributes (these classes/interfaces are present in older Magento versions)
+ */
+use Magento\Catalog\Api\Data\ProductAttributeInterface;
+use Magento\Catalog\Model\FilterProductCustomAttribute;
+use Magento\Catalog\Model\Product;
+use Magento\CatalogGraphQl\Model\ProductDataProvider;
+use Magento\Eav\Api\Data\AttributeInterface;
+
+/**
  *
- * Format a product's custom attribute information to conform to GraphQL schema representation
+ * Format a product's custom attribute information to conform to GraphQL schema representation.
+ * This class returns product's custom attributes as JSON string, even if some of them are null.
+ * Only works in Magento v. 2.4.7. In older versions the response is always 'null' (as string).
  */
 class ProductCustomAttributesLuigi implements ResolverInterface
 {
     /**
-     * @var GetAttributeValueInterface
+     * These attributes are used in Magento v. 2.4.7, because these class and interface only exists from that version
      */
-    private GetAttributeValueInterface $getAttributeValue;
+    private ?\Magento\EavGraphQl\Model\Output\Value\GetAttributeValueInterface $getAttributeValue = null;
+    private ?\Magento\EavGraphQl\Model\Resolver\GetFilteredAttributes $getFilteredAttributes = null;
 
     /**
      * @var ProductDataProvider
@@ -36,31 +44,31 @@ class ProductCustomAttributesLuigi implements ResolverInterface
     private ProductDataProvider $productDataProvider;
 
     /**
-     * @var GetFilteredAttributes
-     */
-    private GetFilteredAttributes $getFilteredAttributes;
-
-    /**
      * @var FilterProductCustomAttribute
      */
     private FilterProductCustomAttribute $filterCustomAttribute;
 
     /**
-     * @param GetAttributeValueInterface $getAttributeValue
      * @param ProductDataProvider $productDataProvider
-     * @param GetFilteredAttributes $getFilteredAttributes
      * @param FilterProductCustomAttribute $filterCustomAttribute
      */
     public function __construct(
-        GetAttributeValueInterface $getAttributeValue,
         ProductDataProvider $productDataProvider,
-        GetFilteredAttributes $getFilteredAttributes,
         FilterProductCustomAttribute $filterCustomAttribute
     ) {
-        $this->getAttributeValue = $getAttributeValue;
         $this->productDataProvider = $productDataProvider;
-        $this->getFilteredAttributes = $getFilteredAttributes;
         $this->filterCustomAttribute = $filterCustomAttribute;
+
+        // Check if Magento version has the necessary classes/interfaces
+        if (interface_exists(\Magento\EavGraphQl\Model\Output\Value\GetAttributeValueInterface::class)) {
+            $this->getAttributeValue = \Magento\Framework\App\ObjectManager::getInstance()
+                ->get(\Magento\EavGraphQl\Model\Output\Value\GetAttributeValueInterface::class);
+        }
+
+        if (class_exists(\Magento\EavGraphQl\Model\Resolver\GetFilteredAttributes::class)) {
+            $this->getFilteredAttributes = \Magento\Framework\App\ObjectManager::getInstance()
+                ->get(\Magento\EavGraphQl\Model\Resolver\GetFilteredAttributes::class);
+        }
     }
 
     /**
@@ -81,55 +89,61 @@ class ProductCustomAttributesLuigi implements ResolverInterface
         array $value = null,
         array $args = null
     ) {
-        $filtersArgs = $args['filters'] ?? [];
+        // If the classes exist, we can compute product's custom attributes
+        if ($this->getAttributeValue && $this->getFilteredAttributes) {
+            $filtersArgs = $args['filters'] ?? [];
 
-        $productCustomAttributes = $this->getFilteredAttributes->execute(
-            $filtersArgs,
-            ProductAttributeInterface::ENTITY_TYPE_CODE
-        );
+            $productCustomAttributes = $this->getFilteredAttributes->execute(
+                $filtersArgs,
+                ProductAttributeInterface::ENTITY_TYPE_CODE
+            );
 
-        $attributeCodes = array_map(
-            function (AttributeInterface $customAttribute) {
-                return $customAttribute->getAttributeCode();
-            },
-            $productCustomAttributes['items']
-        );
+            $attributeCodes = array_map(
+                function (AttributeInterface $customAttribute) {
+                    return $customAttribute->getAttributeCode();
+                },
+                $productCustomAttributes['items']
+            );
 
-        $filteredAttributeCodes = $this->filterCustomAttribute->execute(array_flip($attributeCodes));
+            $filteredAttributeCodes = $this->filterCustomAttribute->execute(array_flip($attributeCodes));
 
-        /** @var Product $product */
-        $product = $value['model'];
-        $productData = $this->productDataProvider->getProductDataById((int)$product->getId());
+            /** @var Product $product */
+            $product = $value['model'];
+            $productData = $this->productDataProvider->getProductDataById((int)$product->getId());
 
-        $customAttributes = [];
-        foreach ($filteredAttributeCodes as $attributeCode => $value) {
-            if (!array_key_exists($attributeCode, $productData)) {
-                continue;
+            $customAttributes = [];
+            foreach ($filteredAttributeCodes as $attributeCode => $value) {
+                if (!array_key_exists($attributeCode, $productData)) {
+                    continue;
+                }
+                $attributeValue = $productData[$attributeCode];
+                if (is_array($attributeValue)) {
+                    $attributeValue = implode(',', $attributeValue);
+                }
+                $customAttributes[] = [
+                    'attribute_code' => $attributeCode,
+                    'value' => $attributeValue
+                ];
             }
-            $attributeValue = $productData[$attributeCode];
-            if (is_array($attributeValue)) {
-                $attributeValue = implode(',', $attributeValue);
-            }
-            $customAttributes[] = [
-                'attribute_code' => $attributeCode,
-                'value' => $attributeValue
-            ];
+
+            return json_encode([
+                'items' => array_map(
+                    function (array $customAttribute) {
+                        if (!is_null($customAttribute['value'])) {
+                            return $this->getAttributeValue->execute(
+                                ProductAttributeInterface::ENTITY_TYPE_CODE,
+                                $customAttribute['attribute_code'],
+                                $customAttribute['value']
+                            );
+                        };
+                    },
+                    $customAttributes
+                ),
+                'errors' => $productCustomAttributes['errors']
+            ]);
         }
 
-        return [
-            'items' => array_map(
-                function (array $customAttribute) {
-                    if (!is_null($customAttribute['value'])) {
-                        return $this->getAttributeValue->execute(
-                            ProductAttributeInterface::ENTITY_TYPE_CODE,
-                            $customAttribute['attribute_code'],
-                            $customAttribute['value']
-                        );
-                    };
-                },
-                $customAttributes
-            ),
-            'errors' => $productCustomAttributes['errors']
-        ];
+        // Fallback for older Magento versions
+        return "null";
     }
 }
